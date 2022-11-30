24b3e152b8c202c1711adff030ccf1851fbe64fa
Steffen Kroggel <kroggel@rkw.de>
2022-11-30 12:13:14 +0100

    [CHANGE] Cleanup for TYPO3 v9.5

--- Classes/TCA/OptionLabels.php
+++ Classes/TCA/OptionLabels.php
@@ -22,7 +22,7 @@ use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
  * Class OptionLabels
  *
  * @author Steffen Kroggel <developer@steffenkroggel.de>
- * @copyright Rkw Kompetenzzentrum
+ * @copyright RKW Kompetenzzentrum
  * @package RKW_RkwEtracker
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
  */
