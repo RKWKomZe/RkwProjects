9aebb54807744e17fc172f8ae1883c7ae4fb5891
Steffen Kroggel <kroggel@rkw.de>
2024-04-02 17:55:46 +0200

    [FEATURE] Preparing for update to TYPO3 v10

--- ext_localconf.php
+++ ext_localconf.php
@@ -25,6 +25,35 @@ call_user_func(
         );
 
         //=================================================================
+        // Add XClasses for extending existing classes
+        // ATTENTION: deactivated due to faulty mapping in TYPO3 9.5
+        //=================================================================
+        /*
+        // for TYPO3 12+
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\RKW\RkwBasics\Domain\Model\Pages::class] = [
+            'className' => \RKW\RkwProjects\Domain\Model\Pages::class
+        ];
+
+        // for TYPO3 9.5 - 11.5 only, not required for TYPO3 12
+        \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\Object\Container\Container::class)
+            ->registerImplementation(
+                \RKW\RkwBasics\Domain\Model\Pages::class,
+                \RKW\RkwProjects\Domain\Model\Pages::class
+            );
+
+        // for TYPO3 12+
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Extbase\Domain\Model\Category::class] = [
+            'className' => \RKW\RkwProjects\Domain\Model\SysCategory::class
+        ];
+
+        // for TYPO3 9.5 - 11.5 only, not required for TYPO3 12
+        \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\Object\Container\Container::class)
+            ->registerImplementation(
+                \TYPO3\CMS\Extbase\Domain\Model\Category::class,
+                \RKW\RkwProjects\Domain\Model\SysCategory::class
+            );
+        */
+        //=================================================================
         // Add Rootline fields
         //=================================================================
         $rootlineFields = &$GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'];
@@ -32,7 +61,7 @@ call_user_func(
         $rootlineFields .= (empty($rootlineFields))? $newRootlineFields : ',' . $newRootlineFields;
 
     },
-    $_EXTKEY
+    'rkw_projects'
 );
 
 
